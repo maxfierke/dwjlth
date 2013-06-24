4ace55f3578a26ff8b216dbd29753cfa2cda6c54
Max Fierke <max@maxfierke.com>
2013-06-23 20:11:36 -0500

    Waiting for the DOM to be ready is redundant when we're part of the DOM.
    Signed-off-by: Max Fierke <max@maxfierke.com>

--- static/js/query.js
+++ static/js/query.js
@@ -1,30 +1,26 @@
-$(document).ready(function () {
-	$('#search').submit(function (e) {
-		if(e.preventDefault) e.preventDefault(); else e.returnValue = false;
-		$.ajax({
-			url: "/search",
-			dataType: "json",
-			data: {'q': $('#query').val()},
-			success: function (data, textStatus) {
-				console.log(data);
-				$('#notfound').addClass('hidden');
-				$('#result').removeClass('hidden');
-				console.log(data.restaurant)
-				if (data.restaurant.verdict) {
-					$('#verdict').removeClass('text-error');
-					$('#verdict').addClass('text-success');
-					$('#verdict').text('Yes');
-				} else {
-					$('#verdict').removeClass('text-success');
-					$('#verdict').addClass('text-error');
-					$('#verdict').text('No');
-				}
-			},
-			error: function (data, textStatus) {
-				$('#result').addClass('hidden');
-				$('#notfound').removeClass('hidden');
+$('#search').submit(function (e) {
+	if(e.preventDefault) e.preventDefault(); else e.returnValue = false;
+	$.ajax({
+		url: "/search",
+		dataType: "json",
+		data: {'q': $('#query').val()},
+		success: function (data, textStatus) {
+			$('#notfound').addClass('hidden');
+			$('#result').removeClass('hidden');
+			if (data.restaurant.verdict) {
+				$('#verdict').removeClass('text-error');
+				$('#verdict').addClass('text-success');
+				$('#verdict').text('Yes');
+			} else {
+				$('#verdict').removeClass('text-success');
+				$('#verdict').addClass('text-error');
+				$('#verdict').text('No');
 			}
-		});
-		return false;
+		},
+		error: function (data, textStatus) {
+			$('#result').addClass('hidden');
+			$('#notfound').removeClass('hidden');
+		}
 	});
+	return false;
 });
